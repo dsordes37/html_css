62c808518d1c8b3ad77dce33423e0e197938104c
dsordes37 <125225427+dsordes37@users.noreply.github.com>
2023-02-12 22:45:11 -0300

    Initial commit

---
+++ aa/script.js
@@ -0,0 +1,16 @@
+function azul(id){
+    var bt=document.getElementById(id)
+    bt.style.backgroundColor='#62c5ff'
+}
+
+
+function normal(id){
+    var bt=document.getElementById(id)
+    bt.style.backgroundColor='white'
+}
+
+function clica(id){
+    var bt=document.getElementById(id)
+
+    bt.innerText='o'
+}
